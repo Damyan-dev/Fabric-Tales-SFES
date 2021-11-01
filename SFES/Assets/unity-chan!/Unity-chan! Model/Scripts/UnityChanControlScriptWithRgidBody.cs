f771d442eca1c0157ec65594ea4a52a56259c9b4
Damyan-dev <56720248+Damyan-dev@users.noreply.github.com>
2021-11-01 13:13:50 +0000

    Update UnityChanControlScriptWithRgidBody.cs

--- SFES/Assets/unity-chan!/Unity-chan! Model/Scripts/UnityChanControlScriptWithRgidBody.cs
+++ SFES/Assets/unity-chan!/Unity-chan! Model/Scripts/UnityChanControlScriptWithRgidBody.cs
@@ -1,14 +1,9 @@
-﻿//
-// Mecanimのアニメーションデータが、原点で移動しない場合の Rigidbody付きコントローラ
-// サンプル
-// 2014/03/13 N.Kobyasahi
-//
+﻿
 using UnityEngine;
 using System.Collections;
 
 namespace UnityChan
 {
-// 必要なコンポーネントの列記
 	[RequireComponent(typeof(Animator))]
 	[RequireComponent(typeof(CapsuleCollider))]
 	[RequireComponent(typeof(Rigidbody))]
