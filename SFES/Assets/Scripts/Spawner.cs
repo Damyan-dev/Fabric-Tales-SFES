950af1d371af1a5a7b33c51e75d01b00ea67468b
Phil <pwr2001235@gmail.com>
2021-11-15 16:24:28 +0000

    Added spawners for items

--- SFES/Assets/Scripts/Spawner.cs
+++ SFES/Assets/Scripts/Spawner.cs
@@ -28,8 +28,8 @@ public class Spawner : MonoBehaviour
         void Spawn()
         {
             // Find a random index between zero and one less than the number of spawn points.
-            int spawnPoints = Random.Range(0, 5);
-            int cotton = Random.Range(0, 3);
+            int spawnPoints = Random.Range(0, 1);
+            int cotton = Random.Range(0, 1);
 
             // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
             Instantiate(this.cotton[cotton], this.spawnPoints[spawnPoints].position, this.spawnPoints[spawnPoints].rotation);
