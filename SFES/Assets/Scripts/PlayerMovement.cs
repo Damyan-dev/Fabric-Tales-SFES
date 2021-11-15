a3b11634a78b484eb38447e5b1124d9b6a04facd
Damyan-dev <56720248+Damyan-dev@users.noreply.github.com>
2021-11-15 12:37:48 +0000

    Integrated the harvest animation

--- SFES/Assets/Scripts/PlayerMovement.cs
+++ SFES/Assets/Scripts/PlayerMovement.cs
@@ -10,90 +10,79 @@ namespace UnityChan
 
 	public class PlayerMovement : MonoBehaviour
 	{
-
-		public float animSpeed = 1.5f;
-		public float lookSmoother = 3.0f;
-		public bool useCurves = true;
-		public float useCurvesHeight = 0.5f;
 		public float playerSpeed = 4.0f;
 		private CapsuleCollider col;
-		private float orgColHight;
-		private Vector3 orgVectColCenter;
 		private Animator anim;
-		private AnimatorStateInfo currentBaseState;
 
-		//static int idleState = Animator.StringToHash("Base Layer.IDLING");
-		//static int locoState = Animator.StringToHash("Base Layer.WALKING");
 
-		void Start()
+		private void Start()
 		{
 			anim = GetComponent<Animator>();
 			col = GetComponent<CapsuleCollider>();
-			orgColHight = col.height;
-			orgVectColCenter = col.center;
 		}
 
+        private void Update()
+        {
+			Harvest();
+        }
 
-		void FixedUpdate()
+        private void FixedUpdate()
 		{
+			Move();
+		}
+
+	
+
+        private void Move()
+        {
 			float h = Input.GetAxis("Vertical");
 			float v = Input.GetAxis("Horizontal");
-			anim.SetFloat("Speed", v);
-			anim.SetFloat("Direction", h);
-			anim.speed = animSpeed;
-			currentBaseState = anim.GetCurrentAnimatorStateInfo(0);
 
 			Vector3 charMovement = new Vector3(h, 0, v);
 			transform.Translate(charMovement * (playerSpeed * Time.fixedDeltaTime), Space.World);
 
 			if (charMovement != Vector3.zero)
 			{
-				anim.SetFloat("Speed", 1);
-				transform.forward = charMovement;
+				if (anim.GetBool("harvesting") == true)
+                {
+					return;
+                } else if (anim.GetBool("harvesting") == false)
+                {
+					anim.SetBool("walking", true);
+					anim.SetInteger("state", 1);
+					transform.forward = charMovement;
+				}
 			}
 			else
 			{
-				anim.SetFloat("Speed", 0);
+				anim.SetBool("walking", false);
+				anim.SetInteger("state", 0);
 			}
-
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
-				anim.SetBool("Harvest", true);
-            } else
-            {
-				anim.SetBool("Harvest", false);
-            }
-
-            
-			/*velocity = new Vector3 (0, 0, v);		
-			velocity = transform.TransformDirection (velocity);
-			if (v > 0.1) {
-				velocity *= forwardSpeed;	
-			} else if (v < -0.1) {
-				velocity *= backwardSpeed;	
-			}
-				
-			transform.localPosition += velocity * Time.fixedDeltaTime; */
-
-
-			/*if (currentBaseState.fullPathHash == locoState) {
-				if (useCurves) {
-					resetCollider ();
-				}
-			}
-
-		else if (currentBaseState.fullPathHash == idleState) {
-				if (useCurves) {
-					resetCollider ();
-				}
-			}
+			
 		}
 		
-		void resetCollider ()
+		private void Harvest()
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+				if(anim.GetBool("walking") == true)
+                {
+					anim.SetBool("walking", false);
+					anim.SetInteger("state", 0);
+				}
+				else if (anim.GetBool("walking") == false)
+                {
+					StartCoroutine(HarvestRoutine());
+				}
+			}
+        }
+		IEnumerator HarvestRoutine()
 		{
-			col.height = orgColHight;
-			col.center = orgVectColCenter;
-		}*/
+			anim.SetBool("harvesting", true);
+			anim.SetInteger("state", 2);
+			yield return new WaitForSeconds(1);
+			anim.SetInteger("state", 0);
+			anim.SetBool("harvesting", false);
 		}
 	}
 }
