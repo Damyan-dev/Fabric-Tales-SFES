b46a396983fda681ed36f8a18c779574cc0570d7
Phil <pwr2001235@gmail.com>
2022-02-06 15:04:16 +0000

    Created currency system
    2 new scripts one is an example of how to change money other script actually has all the currency system

---
+++ SFES/Assets/Scripts/Currency/AddRemoveMoney.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddRemoveMoney : MonoBehaviour
+{
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(x)
+        //{
+        // camera.GetComponent<playerMoney>().addMoney(10);
+        //}
+    }
+}
---
+++ SFES/Assets/Scripts/Currency/playerMoney.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerMoney : MonoBehaviour
+{
+
+    public int money;
+    public Text moneyText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        money = 100;
+        moneyText.text = money.ToString();
+    }
+
+    void addMoney(int moneyToAdd)
+    {
+        money += moneyToAdd;
+        moneyText.text = money.ToString();
+    }
+
+    void subtractMoney(int moneyToSubtract)
+    {
+        if(money - moneyToSubtract < 0)
+        {
+            Debug.Log("not enough money");
+        }
+        else
+        {
+            money -= moneyToSubtract;
+            moneyText.text = money.ToString();
+        }
+    }
+
+}
