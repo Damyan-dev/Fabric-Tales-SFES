fbe6fffee066666b3308ba964f1ff5b629271dad
Phil <61115794+PhilUchiha@users.noreply.github.com>
2022-04-16 15:33:48 +0100

    Added new environment changes system

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -6,7 +6,30 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public GameObject BadEnvironment;
+    public GameObject AverageEnvironment;
 
+
+    private void Update()
+    {
+        if(slider.value <= 15)
+        {
+            BadEnvironment.SetActive(true);
+            //AverageEnvironment.SetActive(false);
+
+        }
+        if (slider.value > 15 && slider.value <= 30)
+        {
+            AverageEnvironment.SetActive(true);
+            BadEnvironment.SetActive(false);
+        }
+        if (slider.value > 30)
+        {
+            AverageEnvironment.SetActive(false);
+            BadEnvironment.SetActive(false);
+        }
+
+    }
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
